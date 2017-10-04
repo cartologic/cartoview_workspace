5bf13a3f6f17a853db66b4f096fdf072bd1c1807
hisham waleed karam <hisham.karam@cartologic.com>
2017-10-04 12:05:05 +0200

    Initial commit

---
+++ installer.py
@@ -0,0 +1,21 @@
+__author__ = 'hisham Karam'
+
+info = {
+    "title": "Cartoview Workspace",
+    "description": "Cartoview Workspace",
+    "author": 'Cartologic',
+    "home_page": 'http://cartologic.com/cartoview/apps/cartoview_workspace',
+    "help_url": "http://cartologic.com",
+    "tags": ['cartoview', 'workspace'],
+    "licence": 'BSD',
+    "author_website": "http://www.cartologic.com",
+    "single_instance": True
+}
+
+
+def install():
+    pass
+
+
+def uninstall():
+    pass
