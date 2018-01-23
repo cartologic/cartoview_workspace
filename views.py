1a86e6c6d6de1b2c98f6228aab96df9403f35502
amira-mohiey <amira.mohiey@cartologic.com>
2018-01-23 10:54:45 +0200

    get the resources from api instead of database

--- views.py
+++ views.py
@@ -2,7 +2,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from builtins import *
-
 from account.decorators import login_required
 from cartoview.app_manager.models import AppInstance
 from django.shortcuts import render
@@ -19,36 +18,20 @@ standard_library.install_aliases()
 
 @login_required
 def workspace(request):
-    owner = request.user
     created_apps = AppInstance.objects.all()
     maps_count = Map.objects.all().count()
     layers_count = Layer.objects.all().count()
     documents_count = Document.objects.all().count()
     groups_count = GroupProfile.objects.all().count()
-    layers_req = requests.get('http://'+request.get_host()+'/api/layers?owner__username='+owner.username)
-    layer = layers_req.json()
-    maps_req = requests.get('http://'+request.get_host()+'/api/maps?owner__username='+owner.username)
-    maps = maps_req.json()
-    appinstances_req = requests.get('http://'+request.get_host()+'/api/appinstances/?owner__username='+owner.username)
-    apps =  appinstances_req.json()
-    groups_req = requests.get('http://'+request.get_host()+'/api/groups?owner__username='+owner.username)
-    groups = groups_req.json()
-    docs_req = requests.get('http://'+request.get_host()+'/api/documents?owner__username='+owner.username)
-    docs = docs_req.json()
-  
-  
+
     return render(
+
         request,
         template_name='cartoview_workspace/workspace.html',
         context={
-            'my_apps': apps,
-            'my_layers': layer,
-            'created_apps': created_apps,
-            'my_maps': maps,
             'maps_count': maps_count,
             'layers_count': layers_count,
-            "groups": groups,
             "groups_count": groups_count,
-            "documents": docs,
-            "documents_count": documents_count
+            "documents_count": documents_count,
+            "user": request.user.username
         })
