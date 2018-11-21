665b8971d41d580d82a01aa8ec00ce19634f6020
hisham waleed karam <hisham.karam@cartologic.com>
2018-11-21 11:38:49 +0200

    django1.11 comptibility

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 from builtins import *
-from django.conf.urls import patterns, url
+
+from django.conf.urls import url
+from future import standard_library
+
 from .views import workspace
-urlpatterns = patterns(
-    '',
-    url('^$', workspace, name="my_workspace"),)
+
+standard_library.install_aliases()
+urlpatterns = [
+    url('^$', workspace, name="my_workspace"), ]
